57c33ba08ddba37fae9a7c5578047d8fd8a1a20b
CODER812 <71447915+coder652@users.noreply.github.com>
2023-12-23 01:33:17 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,32 +1,51 @@
 import pyttsx3
 import requests
-from pprint import pprint
 from pyttsx3 import speak
 import json
-speak('enter your city sir')
-city = input('Enter your city sir : ')
+import datetime
 
-url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=your api here &units=metric'.format(city)
-
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+speak('enter your location sir ')
+city = input('Enter your Location sir <<.>> ')
+speak('Detail Regarding Location ')
+print('Detail Regarding  Location')
+url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=4c82acb8e80be6c1e9c49b78f86072cb&units=metric'.format(city)
 res = requests.get(url)
-
 data = res.json()
-
 temp = data['main']['temp']
 wind_speed = data['wind']['speed']
-
-latitude = data['coord']['lat']
 longitude = data['coord']['lon']
+latitude = data['coord']['lat']
+humidity = data['main']['temp']
+pressure = data['main']['pressure']
+country = data['sys']['country']
+sunrise = data['sys']['sunrise']
+sunset = data['sys']['sunset']
+time = data['timezone']
+visibility = data['visibility']
 
-description = data['weather'][0]['description']
 
+print('Temperature <<.>> {} degree celcius'.format(temp))
 speak('Temperature : {} degree celcius'.format(temp))
-print('Temperature : {} degree celcius'.format(temp))
-speak('Longitude : {}'.format(longitude))
-print('Longitude : {}'.format(longitude))
-print('Wind Speed : {} meter per second'.format(wind_speed))
+print('Wind Speed <<.>> {} meter per second'.format(wind_speed))
 speak('Wind Speed : {} meter per second'.format(wind_speed))
-print('Latitude : {}'.format(latitude))
+print('Humidity <<.>> {}'.format(humidity))
+speak('Humidity : {}'.format(humidity))
+print('Latitude <<.>> {}'.format(latitude))
 speak('Latitude : {}'.format(latitude))
-speak('Description : {}'.format(description))
-print('Description : {}'.format(description))
+print('Longitude <<.>> {}'.format(longitude))
+speak('Longitude : {}'.format(longitude))
+print('Country <<.>> {}'.format(country))
+speak('country : {}'.format(country))
+print('Pressure <<.>> {}'.format(pressure))
+speak('pressure : {}'.format(pressure))
+print('sunrise <<.>> {}'.format(sunrise))
+speak('sunrise : {}'.format(sunrise))
+print('sunset <<.>> {}'.format(sunset))
+speak('sunset : {}'.format(sunset))
+print('visibility <<.>> {}'.format(visibility))
+speak('visibility : {}'.format(visibility))
+print('Conclusion')
+speak('End of the report')
